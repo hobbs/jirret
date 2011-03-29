8f0b5cfbc2a5aa3e6e76124ffadf6df3d35f7238
Zach Hobbs <zach@tunewiki.com>
2011-03-28 17:23:16 -0700

    Looks like it works

--- jirett-process-hook.py
+++ jirett-process-hook.py
@@ -5,19 +5,92 @@
  
 import xmlrpclib
 import sys
+import ConfigParser
+import commands
+import re
 
-#s = xmlrpclib.ServerProxy('http://jira.atlassian.com/rpc/xmlrpc')
-s = xmlrpclib.ServerProxy('http://jira.tunewiki.net/rpc/xmlrpc')
-auth = s.jira1.login('zach', 'droid1')
+# Gerrit command
+gerrit_cmd = "ssh gerrit gerrit"
+
+config = ConfigParser.RawConfigParser()
+config.read('jirett.ini')
+
+user = config.get('jira', 'user')
+password = config.get('jira', 'pass')
+url = config.get('jira', 'url')
+
+rpc = xmlrpclib.ServerProxy(url)
+auth = rpc.jira1.login(user, password)
 
 def showUsage():
-    print '\nUsage: ' + sys.argv[0] + ' [new|merged|abandoned] <change id>\n\n'
+    print '\nNormal hook usage: ' + sys.argv[0] + ' [new|merged|abandoned] <change id> <url to change>'
+    print '\nTo automatically update projects list in config: ' + sys.argv[0] + ' update-projects'
 
-if (len(sys.argv) != 3):
-    showUsage()
-    exit();
+def updateProjects():
+    projects = rpc.jira1.getProjectsNoSchemes(auth)
+    s = ''
+    for p in projects:
+        s += p['key'] + ','
+
+    s = s.rstrip(',')
+    config.set('jira', 'projects', s)
+
+    with open('jirett.ini', 'wb') as configfile:
+        config.write(configfile)
+        
+    print '\nAdded "' + s + '" to jirett.ini.\n'
+
+def updateTicket(what, id, url):
+    status, out = commands.getstatusoutput(gerrit_cmd + ' query --format TEXT change:' + id + ' limit:1')
+    if (status != 0):
+        print 'Could not run gerrit query command.\n' + out
+        exit()
+    subject = re.search('subject:.*[^\n]', out).group();
+    projects = config.get('jira', 'projects').split(',')
+
+    for p in projects:
+        match = re.search('subject:.*(' + p + '-[0-9]+).*', subject).groups()[0]
+        if (len(match) > 0):
+            break
+    if (len(match) > 0):
+        if what == 'new':
+            message = 'New patchset uploaded'
+        elif what == 'merged':
+            message = 'Change merged'
+        elif what == 'abandoned':
+            message = 'Changset abandonded'
+        else:
+            print 'Illegal argument, stopping: ' + what
+            exit()
+
+        message += ' (' + id + ') at ' + url  
+        rpc.jira1.addComment(auth, match, message)
+        exit()
 
 
-s.jira1.addComment(auth, "SMPA-70", 'Comment added with XML-RPC')
+def main():
+
+    if (sys.argv[1] == 'update-projects'):
+        updateProjects()
+        exit()
+
+    if (len(sys.argv) != 4):
+        showUsage()
+        exit()
+
+    what = sys.argv[1]
+    id = sys.argv[2]
+    url = sys.argv[3]
+
+    if (len(what) > 0 and len(id) > 0 and len(url) > 0):
+        updateTicket(what, id, url)
+    else:
+        showUsage()
+        
+if __name__ == '__main__':
+    main()
+
+
+
+
  
-print "Done!"
